e257f98d2b245fe97a94c0ab87df7e226bf13a5b
Antonio Arias Ureta <kiosco.rock@hotmail.com>
2024-10-20 15:25:29 +0200

    Upgrade tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -21,16 +21,17 @@ def print_board(board):
 def enter_move(board):
     # La función acepta el estado actual del tablero y pregunta al usuario acerca de su movimiento,  
     # verifica la entrada y actualiza el tablero acorde a la decisión del usuario.
-    user_movement = 5
-    position = False
-    while user_movement not in range(1, 10) or conversion_dictionary[user_movement] not in list_free_field:
+    user_movement = 5   # Fijo valor para poder entrar al bucle 'while'.
+    row = 1             # Fijo valor para poder entrar al bucle 'while'.
+    colm = 1            # Fijo valor para poder entrar al bucle 'while'.
+    while user_movement not in range(1, 10) or board[row][colm] in ["O", "X"]:
         try:
             user_movement = int(input("Ingresa tu movimiento: "))
         except:
             print("Debe introducir un número entero de 1 a 9.")
-    board[conversion_dictionary[user_movement][0]][conversion_dictionary[user_movement][1]] = "O"
-    user_list_movements.append((conversion_dictionary[user_movement][0],conversion_dictionary[user_movement][1]))
-    return user_list_movements
+        row = (user_movement-1) // 3 # Obtengo el número de fila.       Ejem: (4-1)//3 = 1 ; (9-1)//3 = 2
+        colm = (user_movement-1) % 3 # Obtengo el número de columna.    Ejem: (4-1)%3  = 0 ; (9-1)%3  = 2
+    board[row][colm] = "O"
 
 def make_list_of_free_fields(board):
     # La función examina el tablero y construye una lista de todos los cuadros vacíos. 
@@ -42,87 +43,61 @@ def make_list_of_free_fields(board):
                 list_free_field.append((i,j))
     return list_free_field
 
-def victory_for(board, sign, cont):
+def victory_for(board, sign):
     # La función analiza el estatus del tablero para verificar si 
     # el jugador que utiliza las 'O's o las 'X's ha ganado el juego.
-    if board[0][0] == board[0][1] and board[0][1] == board[0][2] and board[0][2] == "O":
-        sign = 1
-        return sign, cont
-    elif board[2][0] == board[2][1] and board[2][1] == board[2][2] and board[2][2] == "O":
-        sign = 1
-        return sign, cont 
-    elif board[0][0] == board[1][0] and board[1][0] == board[2][0] and board[2][0] == "O":
-        sign = 1
-        return sign, cont 
-    elif board[0][2] == board[1][2] and board[1][2] == board[2][2] and board[2][2] == "O":
-        sign = 1
-        return sign, cont
-    elif board[0][0] == board[0][1] and board[0][1] == board[0][2] and board[0][2] == "X":
-        sign = 2
-        return sign, cont 
-    elif board[1][0] == board[1][1] and board[1][1] == board[1][2] and board[1][2] == "X":
-        sign = 2
-        return sign, cont 
-    elif board[2][0] == board[2][1] and board[2][1] == board[2][2] and board[2][2] == "X":
-        sign = 2
-        return sign, cont
-    elif board[0][0] == board[1][0] and board[1][0] == board[2][0] and board[2][0] == "X":
-        sign = 2
-        return sign, cont 
-    elif board[0][1] == board[1][1] and board[1][1] == board[2][1] and board[2][1] == "X":
-        sign = 2
-        return sign, cont
-    elif board[0][2] == board[1][2] and board[1][2] == board[2][2] and board[2][2] == "X":
-        sign = 2
-        return sign, cont
-    elif board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[2][2] == "X":
-        sign = 2
-        return sign, cont
-    elif board[2][0] == board[1][1] and board[1][1] == board[0][2] and board[0][2] == "X":
-        sign = 2
-        return sign, cont
+    if sign == "O":         # Si el símbolo es 'O' jugamos con el humano.
+        who = "human"
+    elif sign == "X":       # Si el símbolo es 'X' jugamos con la máquina.
+        who = "machine"
     else:
-        cont += 1
-        return sign, cont
-
+        who = None
+    cross1 = cross2 = True
+    for i in range(len(board)):
+        if board[i][0] == sign and board[i][1] == sign and board[i][2] == sign: # Comprobamos filas.
+            return who
+        if board[0][i] == sign and board[1][i] == sign and board[2][i] == sign: # Comprobamos columnas.
+            return who
+        if board[i][i] != sign:     # Comprobamos diagonal (0, 0) a (2, 2)
+            cross1 = False
+        if board[2-i][i] != sign:   # Comprobamos diagonal (2, 0) a (0, 2)
+            cross2 = False
+    if cross1 or cross2:
+        return who
+    return None           
 
 def draw_move(board):
     # La función dibuja el movimiento de la máquina y actualiza el tablero.
-    machine_movement = 5
-    while conversion_dictionary[machine_movement] not in list_free_field:
-        machine_movement = randrange(1,10)
-    board[conversion_dictionary[machine_movement][0]][conversion_dictionary[machine_movement][1]] = "X"
-    print("La máquina ha elegido el movimiento: ", machine_movement)
-    machine_list_movements.append((conversion_dictionary[machine_movement][0],conversion_dictionary[machine_movement][1]))
-    return machine_list_movements
+    control = len(list_free_field)
+    if control > 0:                                     # Si la lista de posiciones libres no está vacía:
+        machine_movement = randrange(control)           # obtengo una posición aleatoria dentro de esa lista.
+        row, colm = list_free_field[machine_movement]   # Asignamos los valores de la tupla elegida a 'row' y 'colm'.
+        print("Yo he elegido el número: ", board[row][colm])
+        board[row][colm] = "X"
 
+
+
+""" MAIN ------------------------------------------------------------------- """
 
 board = [[1, 2, 3], [4, "X", 6,], [7, 8, 9]]
-user_list_movements = []
-machine_list_movements = [(1, 1)]
-conversion_dictionary = {1:(0, 0), 2:(0, 1), 3:(0, 2), 4:(1, 0), 5:(1, 1), 6:(1, 2), 7:(2, 0), 8:(2, 1), 9:(2, 2)}
-sign = 0
 cont = 0
 
 print_board(board)
 
 while True:
-    if cont == 8:
-        print("Habéis empatado")
+    cont += 1
+    if cont == 5:
+        print("Empate !!!")
         break   
     list_free_field = make_list_of_free_fields(board)
     enter_move(board)
-    print(user_list_movements)
     print_board(board)
-    sign, cont = victory_for(board, sign, cont)
-    if sign == 1:
-        print("Has ganado!!!")
+    if victory_for(board, "O") == "human":
+        print("Has ganado !!!")
         break    
     list_free_field = make_list_of_free_fields(board)
     draw_move(board)
-    print(machine_list_movements)
     print_board(board)
-    sign, cont = victory_for(board, sign, cont)
-    if sign == 2:
-        print("Te ha ganado la máquina, pringao!!!")
+    if victory_for(board, "X") == "machine":
+        print("Te he ganado, pringao !!!")
         break
